caf30cf87c360584f2fd4ee5c4ed8a8c18f87cdf
BSkrypka <bkrypka@luxoft.com>
2018-05-31 17:24:17 +0300

    Improved NaiveSplitter

--- contracts/NaiveSplitter.sol
+++ contracts/NaiveSplitter.sol
@@ -5,23 +5,23 @@ contract NaiveSplitter {
   address public recipient2;
   mapping(address => uint) private ownedMoney;
 
-constructor (address first, address second){
-require(recipient1 != 0 && recipient2 != 0);
-recipient1 = first;
-recipient2 = second;
-}
+  constructor (address first, address second){
+    require(recipient1 != 0 && recipient2 != 0);
+    recipient1 = first;
+    recipient2 = second;
+  }
 
-function sendMoney()public {
-require(recipient1 == msg.sender || recipient2 == msg.sender);
-require( ownedMoney [msg.sender] >0);
-msg.sender.transfer(ownedMoney[msg.sender]);
-}
+  function sendMoney() public {
+    require(recipient1 == msg.sender || recipient2 == msg.sender);
+    require(ownedMoney [msg.sender] > 0);
+    msg.sender.transfer(ownedMoney[msg.sender]);
+  }
 
-function splitFunds() public payable returns (bool success) {
-require(msg.value > 0);
-uint256 amount = msg.value/2;
-ownedMoney [recipient1] += amount;
-ownedMoney [recipient2] += amount;
-return true;
-}
+  function splitFunds() public payable returns (bool success) {
+    require(msg.value > 0);
+    uint256 amount = msg.value / 2;
+    ownedMoney [recipient1] += amount;
+    ownedMoney [recipient2] += amount;
+    return true;
+  }
 }
