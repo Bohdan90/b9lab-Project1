cab672a6162c761dec0c9f90110a69ddf24d3bb6
BSkrypka <bkrypka@luxoft.com>
2018-05-31 17:23:00 +0300

    Improved NaiveSplitter

--- contracts/NaiveSplitter.sol
+++ contracts/NaiveSplitter.sol
@@ -1,10 +1,27 @@
 pragma solidity ^0.4.4;
 
 contract NaiveSplitter {
+  address public recipient1;
+  address public recipient2;
+  mapping(address => uint) private ownedMoney;
 
-  function splitFunds(address recipient1, address recipient2) public payable returns (bool success) {
-    recipient1.transfer(msg.value / 2);
-    recipient2.transfer(msg.value / 2);
-    return true;
-  }
+constructor (address first, address second){
+require(recipient1 != 0 && recipient2 != 0);
+recipient1 = first;
+recipient2 = second;
+}
+
+function sendMoney()public {
+require(recipient1 == msg.sender || recipient2 == msg.sender);
+require( ownedMoney [msg.sender] >0);
+msg.sender.transfer(ownedMoney[msg.sender]);
+}
+
+function splitFunds() public payable returns (bool success) {
+require(msg.value > 0);
+uint256 amount = msg.value/2;
+ownedMoney [recipient1] += amount;
+ownedMoney [recipient2] += amount;
+return true;
+}
 }
